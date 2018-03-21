cd27b27c666e076ef066fb2e039b61b34f2b7b1c
Jared Gilpin <irishkungfu@elementary.lan>
2018-03-21 09:12:03 -0500

    incorporated live pricing into chart

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,4 +1,24 @@
-var exchanges = ["Coinbase"];
+var exchanges = [];
+
+function generateTable(exchangeNames) {
+  for (var i = 0; i < exchangeNames.length; i++) {
+    parsedNames = exchangeNames[i].split("~");
+    console.log(parsedNames);
+    exchanges.push(parsedNames[1]);
+    $("#bit-table").after(
+      "<tr><td id='market-" +
+        parsedNames[1] +
+        "'>" +
+        parsedNames[1] +
+        "</td><td id='price-" +
+        parsedNames[1] +
+        "'>--</td><td id='available-" +
+        parsedNames[1] +
+        "'>--</td></tr>"
+    );
+  }
+  console.log(exchanges);
+}
 
 // Code to extract price from streamer
 var streamUrl = "https://streamer.cryptocompare.com/";
@@ -13,6 +33,7 @@ var socket = io(streamUrl);
 $.getJSON(dataUrl, function(data) {
   currentSubs = data["USD"]["TRADES"];
   console.log(currentSubs);
+  generateTable(currentSubs);
   for (var i = 0; i < currentSubs.length; i++) {
     currentSubsText += currentSubs[i] + ", ";
   }
@@ -31,7 +52,7 @@ var transformData = function(data) {
   var coinfsym = CCC.STATIC.CURRENCY.getSymbol(fsym);
   var cointsym = CCC.STATIC.CURRENCY.getSymbol(tsym);
   var incomingTrade = CCC.TRADE.unpack(data);
-  console.log(incomingTrade);
+  // console.log(incomingTrade);
   var newTrade = {
     Market: incomingTrade["M"],
     Type: incomingTrade["T"],
@@ -53,17 +74,22 @@ var transformData = function(data) {
 };
 
 var displayData = function(dataUnpacked) {
-  var maxTableSize = 30;
-  var length = $("table tr").length;
-  if (dataUnpacked.Market === exchanges[0]) {
-    $("#row-test").html(
-      "<td>" +
-        dataUnpacked.Market +
-        "</td><td>" +
-        dataUnpacked.Price +
-        "</td><td>Delete me</td>"
-    );
+  for (var i = 0; i < exchanges.length; i++) {
+    if (exchanges[i] === dataUnpacked.Market) {
+      $("#price-" + exchanges[i]).html(dataUnpacked.Price);
+
+      //   console.log(
+      //     "Market = " + dataUnpacked.Market + "  price = " + dataUnpacked.Price
+      //   );
+    }
   }
+  // $("#row-test").html(
+  //   "<td>" +
+  //     dataUnpacked.Market +
+  //     "</td><td>" +
+  //     dataUnpacked.Price +
+  //     "</td><td>Delete me</td>"
+  // );
 };
 
 $("#unsubscribe").click(function() {
